cc6ce8bb15f60b227c36d63476145d9530a58b2c
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-02-12 02:59:41 +0200

    Update omni_gtanotexllte.mk

--- omni_gtanotexllte.mk
+++ omni_gtanotexllte.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from gtaxllte device
 $(call inherit-product, device/samsung/gtanotexllte/device.mk)
